5586d2e4f6bfe6828e177889668a93c7e63922a9
steve-ng <steve-ng@users.noreply.github.com>
2020-05-10 20:40:22 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+// prints "hi" in the browser's dev tools console
+console.log('hi');
+
+// Instruction from https://materializecss.com/navbar.html
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, {});
+});
