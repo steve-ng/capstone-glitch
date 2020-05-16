3e93502d521dfd60d9f824c8416b5224b2fb87c7
steve-ng <steve-ng@users.noreply.github.com>
2020-05-16 11:52:57 +0800

    update capstone

--- script.js
+++ script.js
@@ -4,6 +4,9 @@
 // prints "hi" in the browser's dev tools console
 console.log('hi');
 
+// Instruction from https://materializecss.com/auto-init.html to initialize
+M.AutoInit();
+
 // Instruction from https://materializecss.com/navbar.html
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
